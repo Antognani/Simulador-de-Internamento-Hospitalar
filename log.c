a88eb8f7bcda6786ed804e007006b035fba62085
Antonio Gabriel <100813360+Antognani@users.noreply.github.com>
2025-07-05 01:14:42 +0000

    add coments and fix identation

--- log.c
+++ log.c
@@ -1,21 +1,19 @@
 #include "log.h"
 
-static FILE* arquivo_log = NULL;
+static FILE* arquivo_log = NULL; // inicializa o ponteiro do arq como null pra evitar erro de acesso
 
 void log_iniciar(const char* nome_arquivo) {
-    arquivo_log = fopen(nome_arquivo, "w");
+    arquivo_log = fopen(nome_arquivo, "w"); //abre o arquivo pra escrita
     if (arquivo_log == NULL) {
         perror("Erro ao abrir arquivo de log");
     }
 }
 
-void log_registrar(const char* mensagem) {
-    // Imprime no terminal
-    printf("%s", mensagem);
+void log_registrar(const char* mensagem) 
+    printf("%s", mensagem); //printa terminal
     // Grava no arquivo
     if (arquivo_log != NULL) {
         fprintf(arquivo_log, "%s", mensagem);
-        fflush(arquivo_log); // Garante que a escrita seja feita imediatamente
+        fflush(arquivo_log); // garante que a escrita seja feita imediatamente
     }
 }
-// ...
